c4ba404e01e9b42ffc8f025861cbb53f24d09ebb
sarit <cs.sarit@gmail.com>
2017-12-06 13:43:41 +0700

    Bug fixed

--- studymptt/genres/views.py
+++ studymptt/genres/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from genres.models import Genre
@@ -8,3 +7,28 @@ class GenreListView(ListView):
     model = Genre
     template_name = 'genre_list.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """Get the context for this view."""
+        queryset = object_list if object_list is not None else self.object_list
+        page_size = self.get_paginate_by(queryset)
+        context_object_name = self.get_context_object_name(queryset)
+        if page_size:
+            paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, page_size)
+            context = {
+                'paginator': paginator,
+                'page_obj': page,
+                'is_paginated': is_paginated,
+                'object_list': queryset
+            }
+        else:
+            context = {
+                'paginator': None,
+                'page_obj': None,
+                'is_paginated': False,
+                'object_list': queryset
+            }
+        if context_object_name is not None:
+            context[context_object_name] = queryset
+        context.update(kwargs)
+        context['nodes'] = context.get('object_list')
+        return super().get_context_data(**context)
