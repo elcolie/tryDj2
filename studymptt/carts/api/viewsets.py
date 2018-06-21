46b5762fc6e3c7283b55df13046b23495b3c1d5e
Sarit Ritwirune <sarit@elcolie.com>
2018-06-21 12:16:38 +0700

    Ready to answer

--- studymptt/carts/api/viewsets.py
+++ studymptt/carts/api/viewsets.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from carts.api.serializers import CartSerializer
 from carts.models import Cart
 
 
 class CartViewSet(viewsets.ModelViewSet):
-    permission_classes = ()
+    permission_classes = (IsAuthenticated,)
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
--- studymptt/carts/tests.py
+++ studymptt/carts/tests.py
@@ -1,8 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from model_mommy import mommy
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from carts.models import Cart
+from items.models import Item
+
+User = get_user_model()
+
 
 class TestCart(TestCase):
     def setUp(self):
         pass
 
     def test_carts(self):
-        mommy.make
+        john = mommy.make(User, username='John', email='john@example.com', password='john1234')
+        client = APIClient()
+        client.force_authenticate(user=john)
+        mommy.make(Item, _quantity=5)
+        url = reverse('api:cart-list')
+        data = {
+            'order_items': [item.id for item in Item.objects.all()],
+            'subtotal': "0.00",
+            'tax_percentage': "0.00",
+            'tax_total': "0.00",
+            'total': "0.00",
+        }
+        res = client.post(url, data=data, format='json')
+        cart = Cart.objects.first()
+        assert 1 == Cart.objects.count()
+        assert 5 == cart.order_items.count()
+        assert status.HTTP_201_CREATED == res.status_code
